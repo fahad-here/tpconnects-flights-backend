b7bdd738d7fa3529440c3cfa74608f5ade6b04c9
fahad_laptop <fmohajir@gmail.com>
2020-10-01 15:32:08 +0400

    Added user roles constant and expiration periods

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,21 @@ const PERMISSION_STATES = {
     FORBIDDEN: 'Forbidden'
 }
 
+const USER_ROLES = {
+    SUPER_ADMIN: 'Super Admin',
+    ADMIN: 'Admin',
+    MANAGER: 'Manager',
+    USER: 'User'
+}
+
+const EXPIRATION_PERIOD = {
+    SHORT: 604800000, // 7 days
+    MEDIUM: 1209600000, // 14 days
+    LONG: 2592000000 // 30 days
+}
+
 module.exports = {
-    PERMISSION_STATES
+    PERMISSION_STATES,
+    USER_ROLES,
+    EXPIRATION_PERIOD
 }
--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,11 +1,13 @@
 const UserSchema = require('./user')
 const RoleSchema = require('./role')
 const PermissionSchema = require('./permission')
+const RolePermissionSchema = require('./role_permission')
 
 const DBSchemas = {
     UserSchema,
     RoleSchema,
-    PermissionSchema
+    PermissionSchema,
+    RolePermissionSchema
 }
 
 module.exports = DBSchemas
